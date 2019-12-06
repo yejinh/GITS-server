9bdff62fa68e5b2f3427d03e6aede0b336df8ec3
Hyejin <wnr212@gmail.com>
2019-12-05 22:44:27 -0500

    Add authenticate router

---
+++ routes/api/controllers/auth.controller.js
@@ -0,0 +1,44 @@
+const jwt = require('jsonwebtoken');
+const User = require('../../../models/User');
+const { vaildEmail } = require('../../../constants/regex');
+
+exports.authenticate = async (req, res, next) => {
+  try {
+    const { email, name, profilePhoto } = req.body;
+    console.log(req.body);
+    if (!vaildEmail.test(email)) {
+      throw new Error('Invaild email');
+    }
+
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      const userData = await new User({
+        email,
+        name,
+        profile_photo: profilePhoto,
+        my_stories: [],
+        favorites: [],
+        history: []
+      }).save();
+
+      const { _id } = userData;
+      const token = jwt.sign({ email, name, _id }, process.env.SECRET_KEY, { expiresIn: '7d' });
+
+      return res.json({
+        message: 'logged in successfully',
+        access_token: token
+      });
+    }
+
+    const token = jwt.sign({ email, name, _id: user._id }, process.env.SECRET_KEY, { expiresIn: '7d' });
+
+    res.json({
+      message: 'logged in successfully',
+      access_token: token
+    });
+  } catch(err) {
+    console.log(err);
+    next(new Error(err));
+  }
+};
