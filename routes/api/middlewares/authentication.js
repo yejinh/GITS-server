d548ec1731841a7ad21d842a4a5ed97cd1ac5458
Hyejin <wnr212@gmail.com>
2019-12-06 15:01:26 -0500

    Add authentication middleware

---
+++ routes/api/middlewares/authentication.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const User = require('../../../models/User');
+require('dotenv').config();
+
+exports.ensureLoggedIn = async (req, res, next) => {
+  try {
+    const bearerHeader = req.headers['authorization'];
+
+    if (!bearerHeader) throw 'Invalid token';
+
+    const bearerToken = bearerHeader.split(' ')[1];
+    const userData = await jwt.verify(bearerToken, process.env.SECRET_KEY);
+    const user = await User.findById(userData._id, '_id email name profile_photo');
+
+    res.locals.userData = user;
+    next();
+  } catch(err) {
+    next(new Error(err));
+  }
+};
